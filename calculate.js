ec6978adca738338f160e936615cba64ecc694ad
Austin Solache <austinsolache@gmail.com>
2024-05-04 12:02:40 -0700

    First Entry

---
+++ calculate.js
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Color Changing Button</title>
+</head>
+<body>
+    <!-- Include the JavaScript file -->
+    <script src="script.js"></script>
+</body>
+</html>
